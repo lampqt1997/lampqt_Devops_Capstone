c4ec043af123a9bb671504b8d68343bfea0fd6d2
lampqt1997 <lampqt1997@gmail.com>
2023-05-16 19:33:06 +0700

    update gitignore

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 30
+let foo = "abc"
+console.log(foo())
 
 app.get("/", (req, res) => {
     res.send("Hello World! My name is LamPQT");
